9d32f06e40dde9e0fc930f114f24df3ba35b63e9
Gautam Bhat <gautam@bu.edu>
2020-06-12 14:43:04 -0700

    0002: Second commit

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 require("http").createServer((inRequest, inResponse) => {
-    inResponse.end("<h1 style='color:#cc0000;'>Finally, I wrote a server!</h1>");
+    inResponse.end("<h1 style='color:#cc0000;'>Welcome! Your IP Address is " + inRequest.connection.remoteAddress + "</h1><p>If your address begins with `ffff:`, it's an IPv4 address mapped through an IPv6 compliant system. Your IPv4 address, then, is the part of the address that comes after `ffff:`. Enjoy the remainder of your stay on this webpage. Toodles. <p>");
 }).listen(9000);
